87132deac3cc5a9a886761645bab89fee5fec3ce
macrespo42 <macrespo@student.42.fr>
2024-09-07 21:39:39 +0200

    fix: to debug

---
+++ ex04/Normalization.py
@@ -0,0 +1,60 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def train_plot() -> None:
+    df = pd.read_csv("normalize.csv")
+
+    jedi = df.loc[df["knight"] == "Jedi"]
+    sith = df.loc[df["knight"] == "Sith"]
+
+    plt.scatter(
+        sith["Survival"],
+        sith["Deflection"],
+        label="sith",
+        color="lightcoral",
+        alpha=0.3,
+    )
+
+    plt.scatter(
+        jedi["Survival"], jedi["Deflection"], label="jedi", color="blue", alpha=0.3
+    )
+
+    plt.legend()
+    plt.show()
+
+    plt.scatter(
+        sith["Empowered"],
+        sith["Stims"],
+        label="sith",
+        color="lightcoral",
+        alpha=0.3,
+    )
+
+    plt.scatter(jedi["Empowered"], jedi["Stims"], label="jedi", color="blue", alpha=0.3)
+
+    plt.legend()
+    plt.show()
+
+
+def normalize(df: pd.DataFrame, columns) -> pd.DataFrame:
+    for column in columns:
+        x = df[column]
+        df.loc[:, column] = (x - x.min()) / (x - x.max())
+    return df
+
+
+def main() -> None:
+    df = pd.read_csv("Train_knight.csv")
+
+    column_to_scale = list(df.columns)
+    if "knight" in column_to_scale:
+        column_to_scale.remove("knight")
+
+    scaled_data = normalize(df, column_to_scale)
+    scaled_data.to_csv("normalize.csv")
+    train_plot()
+
+
+if __name__ == "__main__":
+    main()
---
+++ ex05/separate.py
@@ -0,0 +1,42 @@
+import math
+import sys
+
+from pandas import read_csv
+
+
+def separate(path: str, name="data", train_percentage=80) -> None:
+    if train_percentage >= 99:
+        raise ValueError("The train percentage is to high")
+    df = read_csv(path)
+    train_stop_idx = math.floor((train_percentage / 100) * df.shape[0])
+
+    data_train = df.iloc[0:train_stop_idx]
+    data_test = df.iloc[train_stop_idx::]
+
+    train_path = f"datasets/{name}_train"
+    test_path = f"datasets/{name}_test"
+
+    try:
+        data_train.to_csv(train_path)
+        data_test.to_csv(test_path)
+    except Exception:
+        print("Cannot save split data. Try running script from the root directory")
+        sys.exit(1)
+
+    print(f"training data saved at: {train_path}")
+    print(f"test data saved at: {test_path}")
+
+
+def main() -> None:
+    if not len(sys.argv) == 2:
+        raise ValueError(
+            "Please provide the dataset to separate as argument of this script"
+        )
+    separate(sys.argv[1])
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except ValueError as e:
+        print(f"Error: {e}")
