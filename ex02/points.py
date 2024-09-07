362a557da9294f7ee970ae4955925ed660a23f38
macrespo42 <macrespo@student.42.fr>
2024-09-07 21:22:47 +0200

    feat: ex02 done

--- ex02/points.py
+++ ex02/points.py
@@ -6,21 +6,54 @@ def test_plot() -> None:
     df = pd.read_csv("Test_knight.csv")
 
     plt.scatter(df["Empowered"], df["Stims"], label="Knight", color="green", alpha=0.3)
-    plt.scatter(
-        df["Survival"], df["Deflection"], label="Knight", color="green", alpha=0.3
-    )
     plt.legend()
     plt.show()
 
     plt.scatter(
         df["Survival"], df["Deflection"], label="Knight", color="green", alpha=0.3
     )
+    plt.legend()
+    plt.show()
+
+
+def train_plot() -> None:
+    df = pd.read_csv("Train_knight.csv")
+
+    jedi = df.loc[df["knight"] == "Jedi"]
+    sith = df.loc[df["knight"] == "Sith"]
+
+    plt.scatter(
+        sith["Survival"],
+        sith["Deflection"],
+        label="sith",
+        color="lightcoral",
+        alpha=0.3,
+    )
+
+    plt.scatter(
+        jedi["Survival"], jedi["Deflection"], label="jedi", color="blue", alpha=0.3
+    )
+
+    plt.legend()
+    plt.show()
+
+    plt.scatter(
+        sith["Empowered"],
+        sith["Stims"],
+        label="sith",
+        color="lightcoral",
+        alpha=0.3,
+    )
+
+    plt.scatter(jedi["Empowered"], jedi["Stims"], label="jedi", color="blue", alpha=0.3)
+
     plt.legend()
     plt.show()
 
 
 def main() -> None:
-    test_plot()
+    # test_plot()
+    train_plot()
 
 
 if __name__ == "__main__":
